45a90ee7c11653dff6c2cbd3ffe1edcaa41a1342
myoharin <myoharin@gmail.com>
2026-09-15 18:26:28 +0100

    Added encryption tool and env-xample

--- DEVELOPME.md
+++ DEVELOPME.md
@@ -29,4 +29,6 @@ ideas
 - answer box colour changes from purple(sins) to golden yellow(quotes)
 - devil pages are red and have giant titles
 - celestial pages have a soft color grading
-- 
+
+If you want to encrypt the rewards, the long key must be full lowercase no space only letters combination of all the sins in order. You can use `/judgement-upon-celestials/encrypt` page to assist this.
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Root from './general-pages/root'
+import Encrypt from './general-pages/encrypt'
 
 import './index.css'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Root />} />
+        <Route path="/encrypt" element={<Encrypt />} />
         <Route path="/*" element={<Navigate to="/" replace />}/>
       </Routes>
     </BrowserRouter>
